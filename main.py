1f2a6539a2dae5ed8b9c591eabc2b465f2e0b288
Raymond Wang <49207025+wheatsnackbread@users.noreply.github.com>
2024-03-05 21:45:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-import moviepy.editor as mp
 import whisper
 import time
 import os
+
+model = whisper.load_model("base")  # base, tiny, medium, large
+result = model.transcribe("/Users/raymondwang/Downloads/deepblue.mp4")
+print(result["text"])
+
+# while(True):
+#     i = []
 
 dirname = os.path.dirname(__file__)
 og = os.path.dirname(__file__)
 
 # print(dirname)
 
-os.chdir(dirname + '/vids/')
+os.chdir(dirname + "/vids/")
 
 vidlist = os.listdir()
 
@@ -21,19 +27,21 @@ converttracks = False
 start_time = time.time()
 if converttracks:
     for vid in vidlist:
-        if vid[0] != '.':
-            filename = os.path.join(og + '/vids', vid)
+        if vid[0] != ".":
+            filename = os.path.join(og + "/vids", vid)
             print(filename)
 
             my_clip = mp.VideoFileClip(filename)
-            trackname = os.path.join(og + '/tracks/' + vid[:-4] + ".mp3")
+            trackname = os.path.join(og + "/tracks/" + vid[:-4] + ".mp3")
             my_clip.audio.write_audiofile(trackname)
-    print("\n\n\n Completed audio extraction in ---%s seconds" %
-          (time.time() - start_time))
+    print(
+        "\n\n\n Completed audio extraction in ---%s seconds"
+        % (time.time() - start_time)
+    )
 
 # print(os.listdir())
 
-os.chdir(og + '/tracks/')
+os.chdir(og + "/tracks/")
 
 # print(os.path.dirname())
 
@@ -51,10 +59,10 @@ counter = 0
 
 if transcribe:
     for track in tracklist:
-        if track[0] != '.':
-            print('\n\n\n')
+        if track[0] != ".":
+            print("\n\n\n")
             start_time = time.time()
-            filename = os.path.join(og + '/tracks', track)
+            filename = os.path.join(og + "/tracks", track)
             print(filename)
             print("WhisperAI – Begin processing.")
             model = whisper.load_model("base")  # base, tiny, medium, large
@@ -68,6 +76,11 @@ if transcribe:
             print("WhisperAI – Done.")
             print("Completed in ---%s seconds" % (time.time() - start_time))
             counter += 1
-            print("Processed " + str(counter) + " out of " +
-                  str((len(tracklist)-1)) + " files.")
-            print('\n\n\n')
+            print(
+                "Processed "
+                + str(counter)
+                + " out of "
+                + str((len(tracklist) - 1))
+                + " files."
+            )
+            print("\n\n\n")
